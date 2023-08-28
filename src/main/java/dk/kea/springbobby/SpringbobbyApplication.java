0833e97bc7b57f19a53ce91fca3f6ed908725d3f
nicolaiandersson <nico602d@stud.kea.dk>
2023-08-28 10:56:42 +0200

    -

---
+++ src/main/java/dk/kea/springbobby/SpringbobbyApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.springbobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbobbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbobbyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/springbobby/SpringbobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.springbobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbobbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
